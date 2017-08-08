7e7928d6584561fb38077b7a5f1da31415c817ea
Alessandro Muresan <alessandro.muresan@gmail.com>
2017-08-08 20:21:35 +0300

    Build for gh pages instead of deploying

--- Build-GithubPages.ps1
+++ Build-GithubPages.ps1
@@ -1,15 +1,6 @@
 
 function Main {
 
-    # checkout gh-pages
-    git checkout gh-pages
-
-    # get the latest version
-    git pull origin gh-pages
-
-    # reset the last "Publish" commit
-    git reset --hard HEAD~1
-
     # build the app for production
     npm run build:prod
 
@@ -18,12 +9,6 @@ function Main {
 
     # copy all items from the "dist" directory to the repo root
     Copy-DirectoryContents .\dist $PSScriptRoot
-
-    # create a "Publish" commit
-    git commit -m "Publish"
-
-    # bite the pillow and force push
-    git push --force origin gh-pages
 }
 
 function Copy-DirectoryContents($from, $to) {
